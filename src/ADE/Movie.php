a38b87197d195cc18be4701c3a57824a9176df51
Ronen Hayun <ronen.hayun@weizmann.ac.il>
2015-07-18 23:51:53 +0300

    Fix according to new design

--- src/ADE/Movie.php
+++ src/ADE/Movie.php
@@ -42,7 +42,7 @@ class Movie
     public function getRuntime()
     {
        try {
-            $runtime = $this->getCrawler()->filterXpath("//strong[contains(string(.), 'Length')]/following-sibling::text()[1]")->text();
+            $runtime = $this->getCrawler()->filterXpath("//small[contains(string(.), 'Length')]/following-sibling::text()[1]")->text();
             return $runtime ? $runtime : NULL;
         } catch (\Exception $e) {
             return null;
@@ -52,7 +52,7 @@ class Movie
     public function getReleaseDate()
     {
         try {
-            $date = $this->getCrawler()->filterXpath("//strong[contains(string(.), 'Released')]/following-sibling::text()[1]")->text();
+            $date = $this->getCrawler()->filterXpath("//small[contains(string(.), 'Released')]/following-sibling::text()[1]")->text();
             return $date ? $date : NULL;
         } catch (\Exception $e) {
             return null;
@@ -72,7 +72,7 @@ class Movie
     public function getYear()
     {
         try {
-            $year = $this->getCrawler()->filterXpath("//strong[contains(string(.), 'Production Year')]/following-sibling::text()[1]")->text();
+            $year = $this->getCrawler()->filterXpath("//small[contains(string(.), 'Production Year')]/following-sibling::text()[1]")->text();
             return $year ? $year : NULL;
         } catch (\Exception $e) {
             return null;
@@ -82,10 +82,11 @@ class Movie
     public function getSummary()
     {
         try {
-            $summary = '';
-            $this->getCrawler()->filterXpath("//div[@class='Section Synopsis']//p")->each(function ($node, $i) use (&$summary) {
-                $summary .= htmlentities($node->nodeValue);
-            });
+						$summary = '';
+						$this->getCrawler()->filterXpath("//h4[contains(concat(' ',normalize-space(@class),' '),' synopsis ')]")->each(function ($node, $i) use (&$summary) {
+							$summary .= trim(strip_tags($node->nextSibling->nodeValue));
+						});
+            //$summary = htmlentities($this->getCrawler()->filterXpath("//h4[@class='spacing-bottom text-white synopsis']//p")->text());
             return trim(html_entity_decode($summary));
         } catch (\Exception $e) {
             return null;
@@ -97,8 +98,8 @@ class Movie
         $genres = array();
 
         try {
-            $this->getCrawler()->filterXpath("//div[@class='Section Categories']/p/a")->each(function ($node, $i) use (&$genres) {
-                $genres[] = trim(strip_tags($node->nodeValue));
+            $this->getCrawler()->filterXpath("//a[@label='Category']")->each(function ($node, $i) use (&$genres) {
+                $genres[] = trim(strip_tags($node->parentNode->nodeValue));
             });
         } catch (\Exception $e) {
         }
@@ -111,7 +112,7 @@ class Movie
         $cast = array();
 
         try {
-            $this->getCrawler()->filterXpath("//div[@class='Section Cast']/ul/li/a[@class='PerformerName']")->each(function ($node, $i) use (&$cast) {
+            $this->getCrawler()->filterXpath("//a[@class='PerformerName']")->each(function ($node, $i) use (&$cast) {
                 $cast[] = trim(strip_tags($node->nodeValue));
             });
         } catch (\Exception $e) {
@@ -122,12 +123,10 @@ class Movie
 
     public function getScreens()
     {
-        $scene_url = str_replace('.com', '.com/scenes', $this->url);
-
         $screens = array();
 
         try {
-            $this->getCrawler($scene_url)->filterXpath("//div[@id='scene']/a[@rel='screenshots']/@href")->each(function ($node, $i) use (&$screens) {
+            $this->getCrawler()->filterXpath("//a[@rel='screenshots']/@href")->each(function ($node, $i) use (&$screens) {
                 $screens[] = trim($node->nodeValue);
             });
         } catch (\Exception $e) {
